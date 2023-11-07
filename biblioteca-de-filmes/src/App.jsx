b4b91985d24f908523354c5b95bccf31b0cec493
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 13:50:46 -0300

    rotas configuradas

--- biblioteca-de-filmes/src/App.jsx
+++ biblioteca-de-filmes/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { link, Outlet } from "react-router-dom";
+import './App.css';
 
 function App() {
   
 
   return (
     <div className="app">
+      <nav id='navbar'>
+        <h2>
+          <link to="/">BlibiotecaFilmes</link>
+        </h2>
+        <link to="/movie/1">movie</link>
+        <link to="/search">Search</link>
+      </nav>
       <h2>Biblioteca de Filmes</h2>
+      <Outlet />
     </div>
   )
 }
