2d15b9ae7e5bf8f16275ee61f0fe9addac7e9fd3
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 15:20:25 -0300

    importando filmes da api e paginas de buscas

--- biblioteca-de-filmes/pages/Home.jsx
+++ biblioteca-de-filmes/pages/Home.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { Form } from 'react-router-dom';
+
+const moviesURL = import.meta.env.VITE_API;
+const apikey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRateMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+         console.log(data);   
+        
+    };
+
+    useEffect(() => {
+        const topRateUrl = `${moviesURL}top_rated?${apikey}`;
+
+        getTopRateMovies(topRateUrl);
+    }, []);
+
+    return
+        <div className="container">
+            <h2 className="title">Melhores Filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>carregando...</p>}
+                {topMovies.lenght > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+            </div>
+        </div>
+
     return (
         <div>Home</div>
     )
