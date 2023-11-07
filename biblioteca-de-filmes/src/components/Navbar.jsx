ee3e03695d941d7ffccc599b29e94a439dda0479
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 17:16:16 -0300

    estruturando NavBar para pesquisa de filmes

--- biblioteca-de-filmes/src/components/Navbar.jsx
+++ biblioteca-de-filmes/src/components/Navbar.jsx
@@ -1,21 +1,44 @@
 import React from "react";
-import { link } from "react-dom";
+import { link, useNavigate} from "react-dom";
 import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+import { useState } from "react";
 
 import "./NavBar.css";
 
 const Navbar = () => {
+
+    const [Search, SetSearch] = usState("")
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!search) return
+
+        navigate(`/search?q=${search}`)
+        setSearch("");
+    }
+
     return (
-        <nav id='navbar'>
-        <h2>
-          <link to="/"><BiCameraMovie />BlibiotecaFilmes</link>
-        </h2>
-        <form typeof="text" placeholder="Busque um filme" />
-        <button type="submit">
-            <BiSearchAlt2 />
-        </button>
-      </nav>
-    )
-}
+    <nav id="navbar">
+    <h2>
+      <Link to="/">
+        <BiCameraMovie /> MoviesLib
+      </Link>
+    </h2>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Busque um filme"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      <button type="submit">
+        <BiSearchAlt2 />
+      </button>
+    </form>
+  </nav>
+    );
+};
 
 export default Navbar;
