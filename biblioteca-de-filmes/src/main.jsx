a71c4579109a89e3431d393b31fb3d75952728c8
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 13:34:17 -0300

    configurando rotas

--- biblioteca-de-filmes/src/main.jsx
+++ biblioteca-de-filmes/src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './App.jsx'
 
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
+
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
