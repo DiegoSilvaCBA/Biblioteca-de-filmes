9f7cdc9b5ddddea2d104b787deaf8d5ae875511a
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 16:53:50 -0300

    estilizando MoviesGrid.css

--- biblioteca-de-filmes/pages/Home.jsx
+++ biblioteca-de-filmes/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import { Form } from 'react-router-dom';
+import MovieCard from "../components/MovieCard";
+
+import './MoviesGrid.css';
 
 const moviesURL = import.meta.env.VITE_API;
 const apikey = import.meta.env.VITE_API_KEY;
