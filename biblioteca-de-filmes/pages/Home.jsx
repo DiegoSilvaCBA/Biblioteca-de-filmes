5efcbedf79887ba8d0dfc29dfed753200e83c64a
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 16:09:53 -0300

    estrutura dos cards

--- biblioteca-de-filmes/pages/Home.jsx
+++ biblioteca-de-filmes/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
             <h2 className="title">Melhores Filmes:</h2>
             <div className="movies-container">
                 {topMovies.length === 0 && <p>carregando...</p>}
-                {topMovies.lenght > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+                {topMovies.lenght > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
             </div>
         </div>
 
--- biblioteca-de-filmes/src/components/movieCard.jsx
+++ biblioteca-de-filmes/src/components/movieCard.jsx
@@ -1,9 +1,20 @@
 import { link } from "react-router-dom"
 
-const MovieCard = () => {
-    return (
-        <div>MovieCard</div>
-    )
+import {FaStar} from "react-icons/fa"
+
+const imageUrl = import.meta.env.VITE_IMG;
+
+const MovieCard = ({movie, showLink = true}) => {
+    return ( 
+    <div className="movie-card">
+        <img src={imageUrl + movie.poster_path} alt={movie.title} />
+        <h2>{movie.title}</h2>
+        <p>
+            <FaStar/> {movie.vote_average}
+        </p>
+        {showLink && <link to={`/movie/${movie.id}`}>Detalhes</link>}
+    </div>  
+    );  
 }
 
 export default MovieCard;
