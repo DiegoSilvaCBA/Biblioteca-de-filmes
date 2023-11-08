79533d7c31ebd2960c0df26ecfec5ad033bb1fcd
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 21:43:52 -0300

    imports para o Movies.jsx

--- biblioteca-de-filmes/pages/Movie.jsx
+++ biblioteca-de-filmes/pages/Movie.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "./Movie.css";
 
 const Movie = () => {
     return (
