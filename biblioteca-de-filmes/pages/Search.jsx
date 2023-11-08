6eac8508278b3bf0da41f2186689543e56d5a3d4
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 21:29:04 -0300

    importando style

--- biblioteca-de-filmes/pages/Search.jsx
+++ biblioteca-de-filmes/pages/Search.jsx
@@ -5,6 +5,8 @@ import MovieCard from '../src/components/movieCard';
 const searchURL = import.meta.env.VITE_SEARCH
 const apiKey = import.meta.env.VITE_API_KEY
 
+import "./MoviesGrid.css"
+
 const Search = () => {
     return (
         <div>Search</div>
