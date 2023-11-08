d76335c9a122ab51444390665bdcd4f94d3a0712
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-11-07 21:40:46 -0300

    Update componetes Search.jsx

--- biblioteca-de-filmes/pages/Search.jsx
+++ biblioteca-de-filmes/pages/Search.jsx
@@ -8,8 +8,32 @@ const apiKey = import.meta.env.VITE_API_KEY
 import "./MoviesGrid.css"
 
 const Search = () => {
+
+    const [searchParams] = useSearchParams();
+
+    const [movies, setMovies] = useState([]);
+    const query = searchParams.get("q");
+  
+    const getSearchedMovies = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    };
+  
+    useEffect(() => {
+      const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`; 
+      
+      getSearchedMovies(searchWithQueryURL);
+    }, [query]);
+
     return (
-        <div>Search</div>
+        <div className="container">
+            <h2 className="title">Resultado para: <span className="query-text">{query}</span></h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>carregando...</p>}
+                {topMovies.lenght > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+            </div>
+        </div>
     )
 };
 
